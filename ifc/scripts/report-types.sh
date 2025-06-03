2f784e2d096440137a986957fb75e748a68670f7
Pablo Mayrgundter <pablo.mayrgundter@gmail.com>
2025-06-03 18:39:57 +0200

    scripts: new geom types

--- ifc/scripts/report-types.sh
+++ ifc/scripts/report-types.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 
-# Collect type names from geom-types.txt
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+GEOM_TYPES="$SCRIPT_DIR/geom-types-new.txt"
+
+# Collect type names from $GEOM_TYPES
 # Mark them as implemented or not
 # Lines look like: IFCTYPE, #IFCTYPE, IFCTYPE,Note...
 
-implemented_list=$(grep -v '^#' geom-types.txt | sed 's/,.*//')
-not_implemented_list=$(grep '^#' geom-types.txt | sed 's/^#//' | sed 's/,.*//')
+# New format: TYPE(,todo?)
+implemented_list=$(grep -v ',todo' $GEOM_TYPES | sed 's/,.*//')
+not_implemented_list=$(grep ',todo' $GEOM_TYPES | sed 's/,.*//')
+
+# Old format: #?TYPE
+#implemented_list=$(grep -v '^#' $GEOM_TYPES | sed 's/,.*//')
+#not_implemented_list=$(grep '^#' $GEOM_TYPES | sed 's/^#//' | sed 's/,.*//')
 
 # Prepare blocks
 echo "=== Implemented Types ==="
